96d7f82ba4185d419253dc9c2327b1b0eb748799
chang <drcxd@sina.com>
2019-04-24 20:25:30 +0800

    add the loop mode

--- main.pl
+++ main.pl
@@ -8,21 +8,35 @@ my $URL = "https://dictionary.cambridge.org/dictionary/english/";
 my $PATTERN = "<b class=\\\"def\\\">.*</b>";
 
 # fist a single pass program
-my $word = shift @ARGV;
-my $request_url = "${URL}${word}";
-my $cmd = "curl -s ${request_url} | grep \"${PATTERN}\"";
-# say "cmd is $cmd";
-my @result = `$cmd`;
-for (@result) {
-    my $line = $_;
-    $line =~ s#\A.*<b class="def">##;
-    $line =~ s#</b>.*\Z##;
-    # print $line;
-    while ($line =~ m/title="(\w+)">/)
-    {
-        my $link = $1;
-        $line =~ s#<a.*?${link}</a>#${link}#;
+# query(shift @ARGV);
+
+
+# then a loop command-line interface
+print "% ";
+while (my $word = <STDIN>)
+{
+    chomp $word;
+    query($word);
+    print "% ";
+}
+
+sub query {
+    my $word = shift @_;
+    my $request_url = "${URL}${word}";
+    my $cmd = "curl -s ${request_url} | grep \"${PATTERN}\"";
+    # say "cmd is $cmd";
+    my @result = `$cmd`;
+    for (@result) {
+        my $line = $_;
+        $line =~ s#\A.*<b class="def">##;
+        $line =~ s#</b>.*\Z##;
         # print $line;
+        while ($line =~ m/title="(\w+)">/)
+        {
+            my $link = $1;
+            $line =~ s#<a.*?${link}</a>#${link}#;
+            # print $line;
+        }
+        print $line;
     }
-    print $line;
 }

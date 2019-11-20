6f5b775ea98fb81c42bd0c855de641df95f1a795
Xiaoduan <drcxd@sina.com>
2019-11-20 00:26:36 -0500

    modify the program to work with current cambridge dict website

--- main.pl
+++ main.pl
@@ -5,7 +5,7 @@ use warnings;
 use 5.010;
 
 my $URL = "https://dictionary.cambridge.org/dictionary/english/";
-my $PATTERN = "<b class=\\\"def\\\">.*</b>";
+my $PATTERN = "<div class=\"def ddef_d db\">.*?</div>";
 my $TEMP_FILE = "DICT_TEMP";
 
 # fist a single pass program
@@ -24,10 +24,10 @@ while (my $word = <STDIN>)
 sub query {
     my $word = shift @_;
     my $request_url = "${URL}${word}";
-    # say "downloading...";    
-    `wget -q $request_url -O $TEMP_FILE`;    
+    # say "downloading...";
+    `wget -q $request_url -O $TEMP_FILE`;
     # say "greping...";
-    my $grepcmd = "grep \"<b class=\\\"def\\\">.*</b>\" $TEMP_FILE";    
+    my $grepcmd = "grep -P \"<div class=\\\"def ddef_d db\\\">.*?</div>\" $TEMP_FILE";
     my @result = `$grepcmd`; # it's weird that the commad must run in a variable instead of unfold directly inside the ``
     my $grep_name_cmd = "grep \"og:url\" $TEMP_FILE";
     my $name = `$grep_name_cmd`;
@@ -35,17 +35,22 @@ sub query {
         $name = $1;
         say $name;
     }
-    
+
     for (@result) {
         my $line = $_;
-        $line =~ s#\A.*<b class="def">##;
-        $line =~ s#</b>.*\Z##;
+        $line =~ s#\A.*<div class="def ddef_d db">##;
+        $line =~ s#</div>.*\Z##;
         # print $line;
         while ($line =~ m/title="(\w+)">/)
         {
             my $link = $1;
             $line =~ s#<a.*?${link}</a>#${link}#;
             # print $line;
+        }
+        while ($line =~ m#<span.*?>(.*?)</span>#) {
+            my $content = $1;
+            $line =~ s#<span.*?>.*?</span>#$content#;
+            # say $line;
         }
         print "# ";
         print $line;
